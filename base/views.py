34649bff8b86bab028ede9fdf0d49b890a070c42
Edward Dume <edwarddume55@gmail.com>
2024-01-11 10:23:55 +0300

    first commit

---
+++ base/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render
+from .models import Project
+# Create your views here.
+def homePage(request):
+    projects = Project.objects.all()
+    return render(request, 'base/home.html', {'projects':projects})
