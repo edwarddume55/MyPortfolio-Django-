0992a8bddb97059590cf1b54ff5813086a71868f
Edward Dume <edwarddume55@gmail.com>
2024-01-17 16:02:00 +0300

    adding styles on the project section

--- base/views.py
+++ base/views.py
@@ -1,13 +1,24 @@
 from django.shortcuts import redirect, render
-from .models import Project,Skill,Tag
-from .forms import ProjectForm
+from .models import Project,Skill,Tag,Message
+from .forms import ProjectForm, MessageForm
+from django.contrib import messages
 # Create your views here.
 def homePage(request):
     projects = Project.objects.all()
     detailedskills = Skill.objects.exclude(body ='')
+
     skills = Skill.objects.filter(body='')
-    context={'projects':projects, 'skills':skills, 'detailedskills':detailedskills}
+
+    form= MessageForm()
+
+    if request.method=='POST':
+        form=MessageForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Your message was sucessfull sent')
+    context={'projects':projects, 'skills':skills, 'detailedskills':detailedskills, 'form':form}
     return render(request, 'base/home.html',context )
+
 
 def projectPage(request,pk):
     project=Project.objects.get(id=pk)
@@ -38,3 +49,16 @@ def editProject(request,pk):
 
     context={'form':form}
     return render(request, 'base/project_form.html',context)
+
+def inboxPage(request):
+    inbox= Message.objects.all().order_by('is_read')
+    unreadCount=Message.objects.filter(is_read=False).count()
+    context={'inbox':inbox, 'unreadCount':unreadCount}
+    return render(request, 'base/inbox.html',context)
+
+def messagePage(request,pk):
+    message= Message.objects.get(id=pk)
+    message.is_read = True
+    message.save()
+    context={'message':message}
+    return render(request, 'base/message.html',context)
