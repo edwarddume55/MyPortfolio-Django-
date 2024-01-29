dc48c4261e27f9706b8317498824fd2def9b5002
Edward Dume <edwarddume55@gmail.com>
2024-01-29 17:34:36 +0300

    adding railways configurations

--- portfoliowebsite/settings.py
+++ portfoliowebsite/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-bv1j9rzr@zx$=y8stji(r2r1caqt+v#6g1$_8_r5!vcy3dgk82
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app','.now.sh','127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
